7f789b8ce361bb1a30ce63a29df40888a2d93258
mfkasim91 <firman.kasim@gmail.com>
2020-03-24 10:54:04 +0000

    memoize utility

---
+++ idcovid19/utils/misc.py
@@ -0,0 +1,15 @@
+from functools import wraps
+
+def memoize(method):
+    """Wrap a function such that its result is computed only once.
+    See https://en.wikipedia.org/wiki/Memoization
+    """
+    @wraps(method)
+    def wrapper(obj) -> np.ndarray:
+        attrname = '_' + method.__name__
+        result = getattr(obj, attrname, None)
+        if result is None:
+            result = method(obj)
+        setattr(obj, attrname, result)
+        return result
+    return wrapper
